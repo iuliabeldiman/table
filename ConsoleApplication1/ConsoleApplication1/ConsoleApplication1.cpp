27e80fd77d38557451a24e0c84142dc782b016b8
Beldiman Rădeanu Diana Iuliana <dianabeldiman72@yahoo.ro>
2017-01-06 16:26:49 +0200

    Snake
    calms

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+void Setup()
+{
+	gameOver = false;
+}
+void Draw()
+{
+
+}
+void Input()
+{
+
+}
+
+void Logic()
+{
+
+}
+int main()
+{
+    return 0;
+}
+
