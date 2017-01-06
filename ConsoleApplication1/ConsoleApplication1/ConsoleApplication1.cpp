91f1a7b9c1ba2aa8cc9cb5b5362fb3a192f1e1d3
Beldiman Rădeanu Diana Iuliana <dianabeldiman72@yahoo.ro>
2017-01-06 16:37:21 +0200

    aaaaa
    aabbb

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,26 +1,27 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 bool gameOver;
-const int width = 20;
-const int height = 20;
-int x, y, fruitX, fruitY, score;
+
+
 void Setup()
 {
-	gameOver = false;
-}
+
+ }
+ 
 void Draw()
 {
 
 }
+
 void Input()
 {
 
 }
-
 void Logic()
 {
 
 }
+
 int main()
 {
     return 0;
